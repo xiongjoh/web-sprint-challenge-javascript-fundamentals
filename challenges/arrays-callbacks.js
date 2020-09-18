570a87038453a59d5d78c2e82710dfb499e25bc4
unknown <xiongjoh@gmail.com>
2020-09-18 10:42:16 -0500

    array-callback request 4 finished

--- challenges/arrays-callbacks.js
+++ challenges/arrays-callbacks.js
@@ -54,7 +54,9 @@ console.log(lowPopulationAnimals);
 The zoos need to know their total animal population across the United States. Find the total population from all the zoos using the .reduce() method. Remember the reduce method takes two arguments: a callback (which itself takes two args), and an initial value for the count.
 
 */
-let populationTotal = 0;
+let populationTotal = zooAnimals.reduce(function(population, animalObject) {
+  return population += animalObject.population;
+}, 0)
 console.log(populationTotal);
 
 
